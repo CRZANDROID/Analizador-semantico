105331b2ca7039c9461ba2842eaf2beb3743c54b
JosueGaldamezC <josuegaldamezc@gmail.com>
2024-04-09 21:41:01 -0600

    Semantico commit

--- analizadorLexico.py
+++ analizadorLexico.py
@@ -2,38 +2,44 @@ import ply.lex as lex
 import ply.yacc as yacc
 
 class LexerAnalyzer:
+    def __init__(self):
+        self.lexer = lex.lex(module=self)
+        self.parser = yacc.yacc(module=self)
+        self.errors = []
+        self.variables = {}
+        self.imp_outputs = []
+        self.in_if_condition = False
+        self.if_condition_result = False
+        self.in_function = False
+        
+
     tokens = [
-        'IDENTIFICADOR', 'TIPO', 'PUNTOYCOMA', 'PALABRA_RESERVADA', 'CORCHETES_ABRE',
-        'CORCHETES_CIERRA', 'PARENTESIS_ABRE', 'PARENTESIS_CIERRA', 'FUNCION', 'CONDICIONAL',
-        'NUMERO', 'LLAVES_ABRE','LLAVES_CIERRA'
+        'IDENTIFICADOR', 'TIPO', 'PUNTOYCOMA', 'NUMERO', 'IGUAL', 'STRING',
+        'FUN', 'MALPH', 'IMP', 'LBRACKET', 'RBRACKET', 'LPAREN', 'RPAREN', 'FUNCION',
+        'VI', 'LBRACE', 'RBRACE', 'GT', 'LT', 'EQ','INCREMENTO', 'WAR'
     ]
 
     t_PUNTOYCOMA = r'\;'
-    t_CORCHETES_ABRE = r'\['
-    t_CORCHETES_CIERRA = r'\]'
-    t_PARENTESIS_ABRE = r'\('
-    t_PARENTESIS_CIERRA = r'\)'
-    t_LLAVES_ABRE = r'\{'
-    t_LLAVES_CIERRA = r'\}'
-    
     t_ignore = ' \t\n'
-    def t_PALABRA_RESERVADA(self, t):
-        r'contenido|Vi|War|Fun|Malph'
-        return t
-    def t_FUNCION(self, t):
-        r'[A-Z][a-z]*'
-        return t
+    t_IGUAL = r'\='
+    t_LBRACKET = r'\['
+    t_RBRACKET = r'\]'
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+    t_LBRACE = r'\{'
+    t_RBRACE = r'\}'
+    t_GT = r'\>'
+    t_LT = r'\<'
+    t_EQ = r'\=\='
+    t_INCREMENTO = r'\+\+'
 
-    def t_CONDICIONAL(self, t):
-        r'\>|\<|\='
-        return t
     def t_TIPO(self, t):
         r'int|string'
         return t
-    
-    
-    def t_IDENTIFICADOR(self, t):
-        r'[a-zA-Z_][a-zA-Z_0-9]*'
+
+    def t_STRING(self, t):
+        r'"[^"]*"'
+        t.value = t.value[1:-1]
         return t
 
     def t_NUMERO(self, t):
@@ -41,14 +47,37 @@ class LexerAnalyzer:
         t.value = int(t.value)
         return t
 
+    def t_FUN(self, t):
+        r'Fun'
+        return t
+
+    def t_VI(self, t):
+        r'Vi'
+        return t
+
+    def t_MALPH(self, t):
+        r'Malph'
+        return t
+    
+    def t_WAR(self, t):
+        r'War'
+        return t
+
+    def t_IMP(self, t):
+        r'imp'
+        return t
+
+    def t_FUNCION(self, t):
+        r'[A-Z][a-zA-Z_0-9]*'
+        return t
+
+    def t_IDENTIFICADOR(self, t):
+        r'[a-z_][a-z_0-9]*'
+        return t
+
     def t_error(self, t):
         print(f"Carácter desconocido '{t.value[0]}'")
         t.lexer.skip(1)
-
-    def __init__(self):
-        self.lexer = lex.lex(module=self)
-        self.parser = yacc.yacc(module=self)
-        self.errors = []
 
     def analyze(self, data):
         self.lexer.input(data)
@@ -62,81 +91,193 @@ class LexerAnalyzer:
 
     def parse(self, data):
         self.errors = []
-        result = self.parser.parse(data)
+        self.parser.parse(data)
         if self.errors:
             return False, self.errors
-        return True, result
+        return True, None
 
     def p_program(self, p):
         '''
         program : expression
+                | program expression
         '''
-        p[0] = p[1]
+        if len(p) == 2:
+            p[0] = [p[1]]
+        else:
+            p[0] = p[1] + [p[2]]
 
     def p_expression(self, p):
         '''
         expression : variable_declaration
-                   | main_method
+                   | variable_assignment
+                   | main_function
                    | function_definition
-                   | while_loop
                    | if_statement
+                   | while_statement
+                   
+                   
         '''
         p[0] = p[1]
-    def p_identificador_o_numero(self, p):
-        '''
-        identificador_o_numero : IDENTIFICADOR
-                           | NUMERO
-        '''
-        p[0] = p[1]
-    def p_condicional_simple_o_doble(self, p):
-        '''
-        condicional_simple_o_doble : CONDICIONAL
-                                   | CONDICIONAL CONDICIONAL
-        '''
-        if len(p) == 2:  # Solo un condicional
-            p[0] = p[1]
-        else:  # Dos condicionales
-            p[0] = (p[1], p[2])
 
-    def p_variable_declaration(self, p):
-        'variable_declaration : IDENTIFICADOR PUNTOYCOMA TIPO'
-        p[0] = ('variable_declaration', p[1])
-
-    def p_main_method(self, p):
-        'main_method : PALABRA_RESERVADA PALABRA_RESERVADA CORCHETES_ABRE CORCHETES_CIERRA PARENTESIS_ABRE PALABRA_RESERVADA PARENTESIS_CIERRA'
-        p[0] = ('main_method')
+    def p_main_function(self, p): 
+        '''
+        main_function : FUN MALPH LBRACKET RBRACKET LPAREN imp_statement RPAREN
+        '''
+        self.in_function = True
+        self.execute_imp(p[6])
+        self.in_function = False
 
     def p_function_definition(self, p):
-        'function_definition : PALABRA_RESERVADA FUNCION CORCHETES_ABRE CORCHETES_CIERRA PARENTESIS_ABRE PALABRA_RESERVADA PARENTESIS_CIERRA'
-        p[0] = ('function_definition', p[2])
+        '''
+        function_definition : FUN FUNCION LBRACKET RBRACKET LPAREN imp_statement RPAREN
+        '''
+        self.in_function = True
+        self.execute_imp(p[6])
+        self.in_function = False
 
-    def p_while_loop(self, p):
-        'while_loop : PALABRA_RESERVADA LLAVES_ABRE identificador_o_numero condicional_simple_o_doble identificador_o_numero LLAVES_CIERRA PARENTESIS_ABRE PALABRA_RESERVADA PARENTESIS_CIERRA'
-        p[0] = ('while_loop', p[3], p[4], p[5])
+    def execute_imp(self, imp_data):
+        action, identifier = imp_data
+        if identifier:
+            if identifier in self.variables:
+                var_info = self.variables[identifier]
+                tipo = var_info.get('tipo', 'Tipo desconocido')
+                valor = var_info.get('valor', 'Valor no asignado')
+                output = f"{identifier} ({tipo}): {valor}"
+            else:
+                output = f"{identifier}: Variable no declarada."
+        else:
+            output = "imp sin variable especificada."
+        self.imp_outputs.append(output)
+    
+    
+    def p_imp_statement(self, p):
+        '''
+        imp_statement : IMP IDENTIFICADOR PUNTOYCOMA
+                    | IMP PUNTOYCOMA
+        '''
+        if len(p) == 4:
+            p[0] = ('imp', p[2])  # Retorna una acción imp con una variable
+        else:
+            p[0] = ('imp', None)  # Retorna una acción imp sin variable
+
+    def convert_if_number(self, operand):
+        try:
+            return int(operand)
+        except ValueError:
+            return operand
+
+    def p_variable_declaration(self, p):
+        '''
+        variable_declaration : IDENTIFICADOR PUNTOYCOMA TIPO IGUAL valor
+                             | IDENTIFICADOR PUNTOYCOMA TIPO
+        '''
+        if len(p) == 6:
+            self.variables[p[1]] = {'tipo': p[3], 'valor': p[5]}
+            p[0] = ('variable_declaration_assignment', p[1], p[3], p[5])
+        else:
+            self.variables[p[1]] = {'tipo': p[3], 'valor': None}
+            p[0] = ('variable_declaration', p[1], p[3])
+
+    def p_variable_assignment(self, p):
+        '''
+        variable_assignment : IDENTIFICADOR PUNTOYCOMA TIPO IGUAL valor
+        '''
+        tipo = p[3]
+        valor = p[5]
+        if tipo == "int":
+            try:
+                valor = int(valor)
+            except ValueError:
+                self.errors.append(f"Error: se esperaba un valor entero para la variable '{p[1]}'.")
+                return
+        self.variables[p[1]] = {'tipo': tipo, 'valor': valor}
+        p[0] = ('variable_assignment', p[1], tipo, valor)
+
+    def p_valor(self, p):
+        '''
+        valor : NUMERO
+              | STRING
+        '''
+        p[0] = p[1]
 
     def p_if_statement(self, p):
         '''
-        if_statement : PALABRA_RESERVADA LLAVES_ABRE identificador_o_numero condicional_simple_o_doble identificador_o_numero LLAVES_CIERRA PARENTESIS_ABRE PALABRA_RESERVADA PARENTESIS_CIERRA
+        if_statement : VI LBRACE condition RBRACE LPAREN imp_statement RPAREN
         '''
-        p[0] = ('if_statement', p[3], p[4], p[5])
+    
+        condition_result = self.evaluate_condition(p[3])
+        print(condition_result)
+        if condition_result:
+            print('si llego')
+            self.execute_imp(p[6])
 
+    def p_condition(self, p):
+        '''
+        condition : IDENTIFICADOR GT IDENTIFICADOR
+                  | IDENTIFICADOR GT NUMERO
+                  | NUMERO GT IDENTIFICADOR
+                  | IDENTIFICADOR LT IDENTIFICADOR
+                  | IDENTIFICADOR LT NUMERO
+                  | NUMERO LT IDENTIFICADOR
+                  | IDENTIFICADOR EQ IDENTIFICADOR
+                  | IDENTIFICADOR EQ NUMERO
+                  | NUMERO EQ IDENTIFICADOR
+        '''
+        p[0] = ('condition', p[1], p[2], p[3])
 
+    def evaluate_condition(self, condition):
+        _, left, operator, right = condition
+
+        
+        def get_value(val):
+            if isinstance(val, str):
+                
+                if val in self.variables:
+                    return self.variables[val]['valor']
+                try:
+                    
+                    return int(val)
+                except ValueError:
+                    
+                    return val
+            else:
+                
+                return val
+
+        left_val = get_value(left)
+        right_val = get_value(right)
+
+        if operator == '>':
+            return left_val > right_val
+        elif operator == '<':
+            return left_val < right_val
+        elif operator == '==':
+            return left_val == right_val
+        else:
+            return False
+        
+    def p_while_statement(self, p):
+        '''
+        while_statement : WAR LBRACE condition RBRACE LPAREN imp_statement INCREMENTO IDENTIFICADOR PUNTOYCOMA RPAREN
+        '''
+        condition_result = self.evaluate_condition(p[3])
+        while condition_result:
+            self.execute_imp(p[6])
+            # Incrementa la variable especificada en p[8]
+            if p[8] in self.variables:
+                if 'valor' in self.variables[p[8]] and isinstance(self.variables[p[8]]['valor'], int):
+                    self.variables[p[8]]['valor'] += 1
+                else:
+                    self.errors.append(f"Error: la variable '{p[8]}' no es un entero o no está definida.")
+            else:
+                self.errors.append(f"Error: la variable '{p[8]}' no está definida.")
+            condition_result = self.evaluate_condition(p[3])
+    
+    
     def p_error(self, p):
         if p:
             error_msg = f"Error de sintaxis en '{p.value}', línea {p.lineno}"
             self.errors.append(error_msg)
             print(error_msg)
         else:
-            print("Error de sintaxis al final del archivo")
             self.errors.append("Error de sintaxis al final del archivo")
-
-if __name__ == "__main__":
-    analyzer = LexerAnalyzer()
-    test_data = "War{val1>val2}(contenido)"
-    success, result = analyzer.parse(test_data)
-    if not success:
-        print("Se encontraron errores:")
-        for error in result:
-            print(error)
-    else:
-        print("Análisis completado sin errores.")
--- view.py
+++ view.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import scrolledtext
-from analizadorLexico import LexerAnalyzer
+from analizadorLexico import LexerAnalyzer 
 
 class Application:
     def __init__(self, master):
@@ -11,43 +11,75 @@ class Application:
         background_color = "#1e1e1e"
         foreground_color = "#d4d4d4"
 
+        # Configuración de frames
+        self.left_frame = tk.Frame(master)
+        self.left_frame.pack(side=tk.LEFT, padx=10, pady=10)
+
+        self.right_frame = tk.Frame(master)
+        self.right_frame.pack(side=tk.RIGHT, padx=10, pady=10)
+
         self.lexer_analyzer = LexerAnalyzer()
 
-        self.input_text = scrolledtext.ScrolledText(master, wrap=tk.WORD, width=60, height=20, font=font_style)
-        self.input_text.pack(pady=10, padx=10)
+        # Configuración del área de texto de entrada
+        self.input_text = scrolledtext.ScrolledText(self.left_frame, wrap=tk.WORD, width=60, height=20, font=font_style)
+        self.input_text.pack(pady=10)
         self.input_text.config(bg=background_color, fg=foreground_color, insertbackground=foreground_color)
 
-        self.analyze_button = tk.Button(master, text="Analyze", command=self.analyze)
+        # Botón para analizar
+        self.analyze_button = tk.Button(self.left_frame, text="Analyze", command=self.analyze)
         self.analyze_button.pack(pady=5)
 
-        self.result_text = scrolledtext.ScrolledText(master, wrap=tk.WORD, width=60, height=20, font=font_style)
-        self.result_text.pack(pady=10, padx=10)
+        # Área para mostrar resultados del análisis
+        self.result_text = scrolledtext.ScrolledText(self.left_frame, wrap=tk.WORD, width=60, height=10, font=font_style)
+        self.result_text.pack(pady=10)
         self.result_text.config(bg=background_color, fg=foreground_color, state='disabled')
         self.result_text.tag_config('error', foreground='red')
-        self.result_text.tag_config('correct', foreground='#32CD32')  # Verdenilo
+        self.result_text.tag_config('correct', foreground='#32CD32')  # Light green
+
+        # Área para mostrar el valor de las variables, en el lado derecho
+        self.vars_text = scrolledtext.ScrolledText(self.right_frame, wrap=tk.WORD, width=30, height=40, font=font_style)
+        self.vars_text.pack(pady=10)
+        self.vars_text.config(bg=background_color, fg=foreground_color, state='disabled')
 
     def analyze(self):
         data = self.input_text.get("1.0", tk.END)
         lex_results = self.lexer_analyzer.analyze(data)
-        success, parse_result_or_errors = self.lexer_analyzer.parse(data)
         
+        # Limpiar resultados previos y salidas
         self.result_text.configure(state='normal')
         self.result_text.delete('1.0', tk.END)
+        self.vars_text.configure(state='normal')
+        self.vars_text.delete('1.0', tk.END)
         
+        # Asegurarse de que imp_outputs está limpio al inicio del análisis
+        self.lexer_analyzer.imp_outputs.clear()
+
+        success, parse_result_or_errors = self.lexer_analyzer.parse(data)
+
         if success:
-            self.result_text.insert(tk.END, "Análisis Sintáctico Completo: Sin Errores\n", 'correct')
+            self.result_text.insert(tk.END, "Syntax Analysis Complete: No Errors\n", 'correct')
         else:
             for error in parse_result_or_errors:
                 self.result_text.insert(tk.END, error + '\n', 'error')
                 
-        self.result_text.insert(tk.END, "Lista de Tokens:\n")
+        self.result_text.insert(tk.END, "Token List:\n")
         for token in lex_results:
             self.result_text.insert(tk.END, f"{token}\n")
-                
+            
         self.result_text.configure(state='disabled')
+        
+        # Actualizar y mostrar salidas de imp en el lado derecho
+        for output in self.lexer_analyzer.imp_outputs:
+            self.vars_text.insert(tk.END, f"{output}\n")
+        self.vars_text.configure(state='disabled')
+
+
+
+        
+        
+        
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = Application(root)
     root.mainloop()
-
